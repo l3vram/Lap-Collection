10ab3b4fb7aa716a2cf348be89557de50fe976b9
Marvel Alvarez Rojas <marvelalvarez89@gmail.com>
2018-11-03 17:08:08 -0300

    Initial Commit

---
+++ Lap Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lap Collection
+//
+//  Created by Marvel Alvarez Rojas on 03/11/2018.
+//  Copyright © 2018 Marvel Alvarez Rojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
